1ed4696c079b39938150d842e4c44385632c63c7
kerwin145 <kerwin145@gmail.com>
2020-11-20 21:56:07 -0500

    TeleOp Mecanum Potential Drive Train
    Working on EasyOpenCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMoveUltimateGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMoveUltimateGoal.java
@@ -1,5 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 //contains the drive methods. Uses robotmap.
-public class RobotMoveUltimateGoal {
+public class RobotMoveUltimateGoal extends LinearOpMode {
+    //one joystick for translation, one for rotation.
+
+    public static void drive(double x, double y, double xTurn){
+        //Translation (includes strafing)
+        //get x and y values.
+       double theta = Math.atan2(y, x);
+       double magnitude = Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
+
+       double pow1, pow2; //pow 1 is for FL and BR motor. pow 2 is for FR and BL motor.
+
+        //rotation
+        pow1 = Math.sin(theta + Math.PI / 4) + xTurn;
+        pow2 = Math.sin(theta - Math.PI / 4) + xTurn;
+
+        if(pow1 > 1){
+            pow1 /= pow1;
+            pow2 /= pow1;
+        }
+
+        else if(pow2 > 1){
+            pow2 /= pow2;
+            pow1 /= pow2;
+        }
+
+        RobotMapUltimateGoal.FL_MotorPow = pow1;
+        RobotMapUltimateGoal.BR_MotorPow = pow1;
+        RobotMapUltimateGoal.FR_MotorPow = pow2;
+        RobotMapUltimateGoal.BL_MotorPow = pow2;
+
+        RobotMapUltimateGoal.FL_Motor.setPower(RobotMapUltimateGoal.FL_MotorPow);
+        RobotMapUltimateGoal.BR_Motor.setPower(RobotMapUltimateGoal.BR_MotorPow);
+        RobotMapUltimateGoal.FR_Motor.setPower(RobotMapUltimateGoal.FR_MotorPow);
+        RobotMapUltimateGoal.BL_Motor.setPower(RobotMapUltimateGoal.BL_MotorPow);
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpUltimateGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpUltimateGoal.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TeleOpUltimateGoal {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Innovo TeleOp 2020", group="Teleop")
+public class TeleOpUltimateGoal extends OpMode {
+
+    @Override
+    public void init() {
+        //add deadzone ?
+
+    }
+
+    @Override
+    public void loop() {
+        RobotMoveUltimateGoal.drive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+    }
 }
